a3eea4ed399415c8e5de058f29a8f5a1541190d1
tejasvijb <jbtejasvi@gmail.com>
2025-10-15 15:37:25 +0530

    setup test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        setupFiles: "./tests/setup.js",
+    },
 });
